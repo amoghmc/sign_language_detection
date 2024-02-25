4c6449120c89a2247b8237118eedb9a1ae477736
amoghmc <amoghmc@github.com>
2024-02-25 22:43:49 +0530

    add data

--- inference_classifier.py
+++ inference_classifier.py
@@ -14,14 +14,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-# labels_dict = {
-#     0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
-#     10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
-#     19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'
-# }
-
 labels_dict = {
-    0: 'A', 1: 'B'
+    0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
+    10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
+    19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'
 }
 
 while True:
@@ -68,6 +64,14 @@ while True:
 
         x2 = int(max(x_) * W) - 10
         y2 = int(max(y_) * H) - 10
+
+        desired_length = 84
+        # Pad data_aux with zeros to achieve the desired length
+        while len(data_aux) < desired_length:
+            data_aux.extend([0.0, 0.0])
+
+        # Truncate data_aux if it exceeds the desired length
+        data_aux = data_aux[:desired_length]
 
         prediction = model.predict([np.asarray(data_aux)])
 
